32740368c2cbf901461c8a59a9e3afd0bca096db
Jason S. McMullan <jason.mcmullan@gmail.com>
2020-03-18 22:58:29 -0400

    cmd/uv3dp: Add zcodex (Zortrax Inkspire) support
    - Fixes #27

--- cmd/uv3dp/machine.go
+++ cmd/uv3dp/machine.go
@@ -22,13 +22,14 @@ type MachineInfo struct {
 // Predefined bed layouts
 var (
 	MachineMap = map[string]MachineInfo{
-		"Anycubic-Photon": {Size: MachineSize{1440, 2560, 68.04, 120.96}},
-		"Elogoo-Mars":     {Size: MachineSize{1440, 2560, 68.04, 120.96}},
-		"EPAX-X1":         {Size: MachineSize{1440, 2560, 68.04, 120.96}},
-		"EPAX-X9":         {Size: MachineSize{1600, 2560, 120.0, 192.0}},
-		"EPAX-X10":        {Size: MachineSize{1600, 2560, 135.0, 216.0}},
-		"EPAX-X133":       {Size: MachineSize{2160, 3840, 165.0, 293.0}},
-		"EPAX-X156":       {Size: MachineSize{2160, 3840, 194.0, 345.0}},
+		"Anycubic-Photon":  {Size: MachineSize{1440, 2560, 68.04, 120.96}},
+		"Elogoo-Mars":      {Size: MachineSize{1440, 2560, 68.04, 120.96}},
+		"EPAX-X1":          {Size: MachineSize{1440, 2560, 68.04, 120.96}},
+		"EPAX-X9":          {Size: MachineSize{1600, 2560, 120.0, 192.0}},
+		"EPAX-X10":         {Size: MachineSize{1600, 2560, 135.0, 216.0}},
+		"EPAX-X133":        {Size: MachineSize{2160, 3840, 165.0, 293.0}},
+		"EPAX-X156":        {Size: MachineSize{2160, 3840, 194.0, 345.0}},
+		"Zortrax-Inkspire": {Size: MachineSize{1440, 2560, 72.0, 128.0}},
 	}
 )
 
