8a057c0d97da2258383fd0fac5f90cd3c00fe944
Jason S. McMullan <jason.mcmullan@gmail.com>
2020-05-14 09:14:22 -0400

    decimate: Add # of Passes and bottom/normal only modes
    Summary:
    - Fixes #36

--- cmd/uv3dp/decimate.go
+++ cmd/uv3dp/decimate.go
@@ -5,6 +5,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/spf13/pflag"
 
 	"github.com/ezrec/uv3dp"
@@ -12,21 +14,53 @@ import (
 
 type DecimateCommand struct {
 	*pflag.FlagSet
+
+	Passes int
+	Bottom bool
+	Normal bool
 }
 
-func NewDecimateCommand() (info *DecimateCommand) {
-	flagSet := pflag.NewFlagSet("info", pflag.ContinueOnError)
+func NewDecimateCommand() (cmd *DecimateCommand) {
+	flagSet := pflag.NewFlagSet("cmd", pflag.ContinueOnError)
 	flagSet.SetInterspersed(false)
 
-	info = &DecimateCommand{
+	cmd = &DecimateCommand{
 		FlagSet: flagSet,
 	}
+
+	cmd.BoolVarP(&cmd.Bottom, "bottom", "b", false, "Decimate bottom layers only")
+	cmd.BoolVarP(&cmd.Normal, "normal", "n", false, "Decimate normal layers only")
+	cmd.IntVarP(&cmd.Passes, "passes", "p", 1, "Number of decimation passes")
+
+	cmd.SetInterspersed(false)
 
 	return
 }
 
-func (info *DecimateCommand) Filter(input uv3dp.Printable) (output uv3dp.Printable, err error) {
-	output = uv3dp.NewDecimatedPrintable(input)
+func (cmd *DecimateCommand) Filter(input uv3dp.Printable) (output uv3dp.Printable, err error) {
+	if cmd.Bottom && cmd.Normal {
+		err = fmt.Errorf("only one of --bottom and --normal may be selected")
+		return
+	}
+
+	dec := uv3dp.NewDecimatedPrintable(input)
+
+	dec.Passes = cmd.Passes
+
+	layers := input.Properties().Size.Layers
+	botCount := input.Properties().Bottom.Count
+
+	if cmd.Bottom {
+		dec.FirstLayer = 0
+		dec.Layers = botCount
+	}
+
+	if cmd.Normal {
+		dec.FirstLayer = botCount
+		dec.Layers = layers - botCount
+	}
+
+	output = dec
 
 	return
 }
