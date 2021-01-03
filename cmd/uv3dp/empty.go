d990134b45f660a90d184c12d76ab68196b13e58
Jason S. McMullan <jason.mcmullan@gmail.com>
2021-01-03 11:20:05 -0500

    uv3dp: Support generating a solid block of grayscale in 'empty'

--- cmd/uv3dp/empty.go
+++ cmd/uv3dp/empty.go
@@ -8,6 +8,10 @@ import (
 	"github.com/spf13/pflag"
 
 	"github.com/ezrec/uv3dp"
+
+	"image"
+	"image/color"
+	"image/draw"
 )
 
 type EmptyFormatter struct {
@@ -16,6 +20,8 @@ type EmptyFormatter struct {
 	Pixels      []int
 	Millimeters []float32
 	Machine     string
+	Gray        byte
+	Layers      int
 }
 
 func NewEmptyFormatter() (ef *EmptyFormatter) {
@@ -26,13 +32,24 @@ func NewEmptyFormatter() (ef *EmptyFormatter) {
 	defaultMachine := uv3dp.MachineFormats["photon"]
 	size := &defaultMachine.Machine.Size
 
+	ef.Uint8VarP(&ef.Gray, "gray", "g", 0, "Grayscale color (0 for black, 255 for white)")
 	ef.IntSliceVarP(&ef.Pixels, "pixels", "p", []int{size.X, size.Y}, "Empty size, in pixels")
 	ef.Float32SliceVarP(&ef.Millimeters, "millimeters", "m", []float32{size.Xmm, size.Ymm}, "Empty size, in millimeters")
-
+	ef.IntVarP(&ef.Layers, "layers", "l", 1, "Number of 0.05mm layers")
 	ef.StringVarP(&ef.Machine, "machine", "M", "photon", "Size preset by machine type")
 	ef.SetInterspersed(false)
 
 	return
+}
+
+type EmptyPrint struct {
+	uv3dp.Print
+
+	Image *image.Gray
+}
+
+func (ep *EmptyPrint) LayerImage(index int) (ig *image.Gray) {
+	return ep.Image
 }
 
 func (ef *EmptyFormatter) Decode(file uv3dp.Reader, filesize int64) (printable uv3dp.Printable, err error) {
@@ -45,6 +62,11 @@ func (ef *EmptyFormatter) Decode(file uv3dp.Reader, filesize int64) (printable u
 	size.Y = msize.Y
 	size.Millimeter.X = msize.Xmm
 	size.Millimeter.Y = msize.Ymm
+	size.LayerHeight = 0.05
+	size.Layers = 1
+
+	prop.Bottom.Exposure.LightPWM = 255
+	prop.Exposure.LightPWM = 255
 
 	if ef.Changed("pixels") {
 		size.X = ef.Pixels[0]
@@ -56,7 +78,17 @@ func (ef *EmptyFormatter) Decode(file uv3dp.Reader, filesize int64) (printable u
 		size.Millimeter.Y = ef.Millimeters[1]
 	}
 
-	printable = &uv3dp.Print{Properties: prop}
+	if ef.Changed("layers") {
+		size.Layers = ef.Layers
+	}
+
+	layerImage := image.NewGray(prop.Bounds())
+	draw.Draw(layerImage, layerImage.Bounds(), &image.Uniform{C: color.Gray{Y: ef.Gray}}, image.ZP, draw.Src)
+
+	printable = &EmptyPrint{
+		Print: uv3dp.Print{Properties: prop},
+		Image: layerImage,
+	}
 
 	return
 }
